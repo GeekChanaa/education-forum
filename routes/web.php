f20134c84da77d33e143b2931b2760e3582196fe
chanaa <mohammedchanaa@hotmail.com>
2020-04-30 00:26:15 +0000

    initial

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use MongoDB\Client as Mongo;
+
+Route::get('mongo', function(Request $request) {
+    $collection = (new Mongo)->edcuation->questions;
+    return $collection->find()->toArray();
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
